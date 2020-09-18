4f7ec520023cc19879bf636377e6d6d14d62ea56
Maheen Unzeelah <46377344+maheenunzeelah@users.noreply.github.com>
2020-09-18 19:48:26 +0500

    broadcast

--- server/index.js
+++ server/index.js
@@ -2,21 +2,29 @@ const express=require('express');
 const http=require('http');
 const socketIO=require('socket.io');
 const router=require('./router')
+const {addUsers,getUser,getUserInRoom,removeUser}=require('./users')
 
 const PORT=process.env.PORT || 5000
 const app=express();
 const server=http.createServer(app)
 const io=socketIO(server);
+
+//When any user connects
+
 io.on('connection',(socket)=>{
     console.log('User connected');
 
+    // Will wait for event `join` from client
     socket.on('join',({name,room},callback)=>{
-      console.log(name,room)
-    //   let error=true;
-    //   if(error){
-    //       callback({error:'this is a error'})
-    //   }
+      const {error, user}=addUsers({id:socket.id,name,room})
+       
+      // if error that means user already exists in that room
+      if(error) return callback(error) 
+      
+      socket.join(user.room)
     })
+
+    //when user disconnects
     socket.on('disconnect',()=>{
         console.log('User has left')
     })
--- server/users.js
+++ server/users.js
@@ -5,23 +5,28 @@ const addUsers = ({ id, name, room }) => {
     name = name.trim().toLowerCase();
     room = room.trim().toLowerCase();
 
+    //if same user tries to join the same room
     const isExisting = users.find(user => user.name === name && user.room === room)
     console.log(isExisting)
+
     if (isExisting)
         return { error: 'User already exists' }
+
     const user = { id, name, room }
     users.push(user);
+
     return { user }
 }
-const removeUser = () => {
+const removeUser = (id) => {
+      const index=users.findIndex((user)=>user.id===id)
 
+      //if user exists
+      if(index !== -1){
+          users.splice(index,1)[0]
+      }
 }
-const getUser = () => {
+const getUser = (id) => users.find(user=>user.id===id)
 
-}
-
-const getUserInRoom = () => {
-
-}
+const getUserInRoom = (room) => users.filter(user=>user.room===room)
 
 module.exports = { addUsers, removeUser, getUser, getUserInRoom };

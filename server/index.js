d6e82dd3ed4b328541a4729defbaa38e654125c3
Maheen Unzeelah <46377344+maheenunzeelah@users.noreply.github.com>
2020-09-19 19:15:06 +0500

    final

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const http=require('http');
 const socketIO=require('socket.io');
 const router=require('./router')
 const {addUsers,getUser,getUserInRoom,removeUser}=require('./users')
-
+const path=require('path')
 const PORT=process.env.PORT || 5000
 const app=express();
 const server=http.createServer(app)
@@ -55,4 +55,6 @@ io.on('connection',(socket)=>{
     })
 })
 app.use(router)
+
+
 server.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
